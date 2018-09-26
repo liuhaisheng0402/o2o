43b3368910d0a1558d18f748c4a672aa13ddc57b
gitee <liuhaisheng0402@163.com>
2018-09-26 22:53:31 +0800

    add by lhs, add time 20180926

---
+++ src/main/java/com/imooc/service/impl/AreaServiceImpl.java
@@ -0,0 +1,21 @@
+package com.imooc.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.imooc.dao.AreaDao;
+import com.imooc.entity.Area;
+import com.imooc.service.AreaService;
+
+@Service
+public class AreaServiceImpl implements AreaService {
+
+	@Autowired
+	private AreaDao areaDao;
+	
+	@Override
+	public List<Area> getAreaList() {
+		return areaDao.queryArea();
+	}
+	
+}
---
+++ src/test/java/com/imooc/service/AreaServiceTest.java
@@ -0,0 +1,19 @@
+package com.imooc.service;
+
+import static org.junit.Assert.assertEquals;
+import java.util.List;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.imooc.BaseTest;
+import com.imooc.entity.Area;
+
+public class AreaServiceTest extends BaseTest {
+	@Autowired
+	private AreaService areaService;
+	
+	@Test
+	public void testGetAreaList() {
+		List<Area> areas = areaService.getAreaList();
+		assertEquals("西苑", areas.get(0).getAreaName());
+	}
+}
